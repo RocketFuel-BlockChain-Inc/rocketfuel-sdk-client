b6e2a304b288727abb070c401e8446d7a1f7eb96
manish2524 <manish.s@rocketfuel.inc>
2025-08-04 19:32:55 +0530

    fix: transact client

--- rollup.config.js
+++ rollup.config.js
@@ -54,7 +54,7 @@ export default defineConfig([
   {
     input: 'src/standalone.ts',
     output: {
-      file: `dist/rocketfuel-client.min-${pkg.version}.js`,
+      file: `dist/rkfl-transact-client.min-${pkg.version}.js`,
       format: 'iife',
       name: 'RkflPlugin',
       banner: versionBanner,
